bad6b5350c33f696b494d53e79fa5d7e6b702f51
Романчук Сергей <rezety@gmail.com>
2019-02-27 00:34:49 +0700

    init

---
+++ Akhmed/Akhmed.cs
@@ -0,0 +1,25 @@
+﻿using Robocode;
+
+namespace Romanchuk
+{
+    public class Akhmed : Robot
+    {
+        public override void Run()
+        {
+            // Perform your initialization for your robot here
+
+            while (true)
+            {
+                this.TurnLeft(45);
+                // Perform robot logic here calling robot commands etc.
+            }
+        }
+
+        // Robot event handler, when the robot sees another robot
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            // We fire the gun with bullet power = 1
+            Fire(1);
+        }
+    }
+}
